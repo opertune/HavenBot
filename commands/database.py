8ff46dca9f1f0c276908c932047f7870afab05bd
Romain <r.baucheron@gmail.com>
2024-02-14 23:24:57 +0100

    Update database.py

--- commands/database.py
+++ commands/database.py
@@ -6,12 +6,15 @@ load_dotenv('.env')
 
 class DB:
     def __init__(self):
-        self.mydb = mysql.connector.connect(
+        try:
+            self.mydb = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
             database=os.getenv('DB_NAME')
         )
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
 
     def querry(self, sqlstmt, data):
         cursor = self.mydb.cursor()
